35a5aa847a2a0494d29becb68513dd8f02926fb5
Your Name <you@example.com>
2023-01-12 23:40:45 +0330

    1.8 completed

--- 1.1_1.8/1.8/main.js
+++ 1.1_1.8/1.8/main.js
@@ -1,5 +1,31 @@
 $(function(){
-    
+
+    $(".slider").eq(0).slideUp();
+    $(".slider").eq(1).slideUp();
+
+     
+    $(".selector").on({
+        click: function(){
+        if($(this).next().is(":hidden")){
+            
+
+            $("i").attr("class","fa fa-caret-right");
+            console.log($(".selector").children().eq(0));
+
+
+        
+
+
+            
+            $(".selector").next().slideUp();
+            $(this).children().eq(0).attr("class","fa fa-caret-down");
+            $(this).next().slideDown();
+        }
+       
+            
+        }
+    })
+
  
 
 })
