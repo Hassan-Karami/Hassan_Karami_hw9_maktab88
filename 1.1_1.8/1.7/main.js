5ed3ed3d354949fe1c80702ba4a0d94d799437ab
Your Name <you@example.com>
2023-01-12 21:19:00 +0330

    7 completed

--- 1.1_1.8/1.7/main.js
+++ 1.1_1.8/1.7/main.js
@@ -1,5 +1,17 @@
+let w=10;
+let l=10;
 $(function(){
+    $("button").on("click",function(){
+        if(l===100 && w===100){
+            alert("You Won");
+            window.location.reload();
+            
+        }
+     
+        $("#load").css("width",`${l+=10}%`);
+        $("p").html(`${w+=10}%`);
+        
+       
+    })
     
- 
-
 })
